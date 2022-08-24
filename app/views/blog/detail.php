9e73f6efd60d385acb4e7e7e10b4fd95003464c9
gnendraaw <itsgnendraaw@gmail.com>
2022-08-24 13:09:24 +0800

    add image on detail page

--- app/views/blog/detail.php
+++ app/views/blog/detail.php
@@ -1,6 +1,6 @@
 <div class="container mt-3">
     <div class="card mb-3">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="<?=BASE_URL?>/img/monster.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title"><?=$data['blog']['judul']?></h5>
     <p class="card-text"><small class="text-muted"><?=$data['blog']['username']?></small></p>
